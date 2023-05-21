2a683e08ef21adc7a3f59ea86cd130bc6e8e4cea
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2023-05-22 00:56:19 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+
+const _stack = new WeakMap();
+
+class Stack {
+    constructor(){_stack.set(this, []);}
+
+    add(item){
+    _stack.get(this).push(item);
+    }
+
+    remove(){ 
+    const stack = _stack.get(this);
+    if (stack.length === 0) return 'The Stack is empty';
+    return stack.pop();
+    }
+
+    last(){
+    const stack = _stack.get(this);
+    if (stack.length === 0) return 'The Stack is empty';
+    return stack[stack.length - 1];
+    }
+
+    count() {
+        return _stack.get(this).length;
+    }
+}
+
+let s = new Stack();
+s.add(1);
+
+
